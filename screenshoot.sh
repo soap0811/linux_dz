61f47447f0103238f5313d02da8a8fa699a720a8
soap0811 <51877224+soap0811@users.noreply.github.com>
2019-08-02 22:38:06 +0300

    Delete screenshoot.sh

--- screenshoot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-scrot  /tmp/screen.png
-curl  -F "upload=yes" -F "filename=@/tmp/screen.png" http://www.radikal.ru/action.aspx | grep "input_link_1\"" | cut -d "\"" -f4 | tee -ai /var/log/screens.log | xsel -b -i ;
-tail -n 1 /var/log/screens.log |  xargs -I _ google-chrome _ ;
-notify-send "Загрузка скриншота" "Скриншот был успешно загружен" -i /usr/share/icons/gnome/scalable/emblems/emblem-default.svg -t 5000 ;
-
-curl  -F "upload=yes" -F "file=@/tmp/screen.png" -L http://habrastorage.org/storage/habraeffect/05/68/056805cc524b8f502bd06f517fb65059.pngupload.php  | grep -e "original-link" |  awk -F"\"" '{print $10}' | tee -ai /var/log/screens.log | xsel -b -i ;
-tail -n 1 /var/log/screens.log |  xargs -I _ google-chrome _ ;
-notify-send "Загрузка скриншота" "Скриншот был успешно загружен" -i /usr/share/icons/gnome/scalable/emblems/emblem-default.svg -t 5000 ; 
